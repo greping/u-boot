758c5ef359d31bae8ca721221a271de7060edad5
greping <ubuntu>
2015-06-17 11:18:07 +0800

    add doublesystem in uboot

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#define CFG_ATHRS27_PHY 1
+#define CFG_AG7240_NMACS 2
+#define CONFIG_AP123 1
+#define CONFIG_AR7240 1
+#define CONFIG_WASP 1
+#define FLASH_SIZE 16
+
+/* Automatically generated - do not edit */
+#include <configs/db12x.h>
